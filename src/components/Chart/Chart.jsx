f18c8c05e54703a55fbd3a893d3d2f19e16498b7
angelly <angelly@i4sea.com>
2020-06-05 11:15:27 -0300

    Gráfico em barras p/quando um país selecionado

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -4,7 +4,7 @@ import { Line, Bar } from "react-chartjs-2";
 
 import styles from "./Chart.module.css";
 
-const Chart = () => {
+const Chart = ({ data: { confirmed, deaths, recovered }, country }) => {
   const [dailyData, setDailyData] = useState([]);
 
   useEffect(() => {
@@ -13,7 +13,7 @@ const Chart = () => {
     };
 
     fetchAPI();
-  });
+  }, []);
 
   const lineChart = dailyData.length ? (
     <Line
@@ -38,7 +38,35 @@ const Chart = () => {
     />
   ) : null;
 
-  return <div className={styles.container}>{lineChart}</div>;
+  console.log(confirmed, recovered, deaths);
+
+  // Gráfico de barras
+  const barChart = confirmed ? (
+    <Bar
+      data={{
+        labels: ["Infectados", "Recuperados", "Óbitos"],
+        datasets: [
+          {
+            label: "Pessoas",
+            backgroundColor: [
+              "rgba(9, 0, 255, 0.5)",
+              "rgba(9, 255, 0, 0.5)",
+              "rgba(255, 0, 0, 0.5)",
+            ],
+            data: [confirmed.value, recovered.value, deaths.value],
+          },
+        ],
+      }}
+      options={{
+        legend: { display: false },
+        title: { display: true, text: `Estado atual em ${country}` },
+      }}
+    />
+  ) : null;
+
+  return (
+    <div className={styles.container}>{country ? barChart : lineChart}</div>
+  );
 };
 
 export default Chart;
